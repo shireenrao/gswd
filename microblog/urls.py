fc0efc71bda2724bea3c30a7aa5974fe9f9dbed2
Srinivas Nyayapati <shireenrao@gmail.com>
2013-06-02 03:41:04 +0000

    Misc

--- microblog/urls.py
+++ microblog/urls.py
@@ -1,13 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'microblog.views.home', name='home'),
-    # url(r'^microblog/', include('microblog.foo.urls')),
     url(r'^$', TemplateView.as_view(template_name='index.html')),
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
