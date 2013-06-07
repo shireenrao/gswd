241bb92d9327dc40219a7dd5291445fe6f548df4
Srinivas Nyayapati <shireenrao@gmail.com>
2013-06-07 21:17:05 +0000

    class based views

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,45 @@
-# Create your views here.
+from django.views.generic import ListView, DetailView
+
+from .models import Post
+
+
+class PublishPostsMixin(object):
+
+    def get_queryset(self):
+    	queryset = super(PublishPostsMixin, self).get_queryset()
+    	return queryset.filter(published=True)
+
+#def blog_list(request, *args, **kwargs):
+#    post_list = Post.objects.filter(published=True)
+#    template_name = "post_list.html"
+#
+#    context = {
+#        "post_list": post_list
+#    }
+#
+#    return render(request, template_name, context)
+
+class PostListView(PublishPostsMixin, ListView):
+    model = Post
+
+    def get_queryset(self):
+    	queryset = super(PostListView, self).get_queryset()
+    	return queryset.filter(published=True)
+
+
+#def blog_detail(request, pk, *args, **kwargs):
+#    post = Post.objects.get(pk=pk, published=True)
+#    template_name = "blog/post_detail.html"
+#
+#    context = {
+#        "post": post
+#    }
+#
+#    return render(request, template_name, context)
+
+class PostDetailView(PublishPostsMixin, DetailView):
+	model = Post
+
+	def get_queryset(self):
+		queryset = super(PostDetailView, self).get_queryset()
+		return queryset.filter(published=True)
--- microblog/urls.py
+++ microblog/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
+#from django.views.generic import TemplateView
+from . import views
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    #url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^$', views.HomepageView.as_view(), name='home'),
+    url(r'^blog/', include("blog.urls", namespace="blog")),
     url(r'^admin/', include(admin.site.urls)),
 )
